17f22484773e7284688f2330f57ea97a55a81c64
Kauan Pinto <kauan.pinto@semtel.pms.ad>
2025-03-20 09:54:52 -0300

    fazendo o escopo da função

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,31 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let aPagar = 0;
+
+  if (days <= 2) {
+    for (let i = 0; i < days; i++) {
+      aPagar += 40;
+    }
+
+    return aPagar;
+  }
+
+  if (days >= 3 && days <= 6) {
+    for (let i = 0; i < days; i++) {
+      aPagar += 40;
+    }
+
+    return aPagar - 20;
+  }
+
+  if (days >= 7) {
+    for (let i = 0; i < days; i++) {
+      aPagar += 40;
+    }
+
+    return aPagar - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
